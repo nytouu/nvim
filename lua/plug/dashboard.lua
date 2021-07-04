370e984abbde4eec3fee6dccbb4469f0ac453e47
nytouu <capitaine7312@gmail.com>
2021-07-04 18:20:17 +0200

    dashboard config

--- lua/plug/dashboard.lua
+++ lua/plug/dashboard.lua
@@ -1,15 +1,15 @@
 vim.g.dashboard_default_executive = 'telescope'
 
 vim.g.dashboard_custom_section = {
-    a = {description = {'  Find File        spc f f'}, command = 'Telescope find_files'},
-    b = {description = {'  New File         spc f n'}, command = ':DashboardNewFile'},
-    c = {description = {'  Recent Files     spc f h'}, command = 'Telescope oldfiles'},
-    d = {description = {'  Live Grep        spc f w'}, command = 'Telescope live_grep'},
-    e = {description = {'漣 Settings         spc f s'}, command = ':e $HOME/.config/nvim/lua/settings.lua'},
-    f = {description = {'  Quit             spc q q'}, command = ':q'}
+	a = {description = {'  File Browser     spc f b'}, command = 'Telescope file_browser'},
+    b = {description = {'  Find File        spc f f'}, command = 'Telescope find_files'},
+    d = {description = {'  Recent Files     spc f h'}, command = 'Telescope oldfiles'},
+    e = {description = {'  Live Grep        spc f w'}, command = 'Telescope live_grep'},
+    f = {description = {'漣 Settings         spc f s'}, command = ':e $HOME/.config/nvim/lua/settings.lua'},
+    g = {description = {'  Quit             spc q q'}, command = ':q'}
 }
 
-vim.g.dashboard_custom_footer = {'hello(world);'}
+-- vim.g.dashboard_custom_footer = {'hello(world);'}
 
 vim.g.dashboard_custom_header =
 -- {
