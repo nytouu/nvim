330a97ab8994b6109d7e47f3e1b15c0cb695a21f
nytouu <capitaine7312@gmail.com>
2021-07-11 00:00:07 +0200

    italic comments

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,6 @@
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_comments = false
+vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_sidebars = { "NvimTree", "Outline", "toggleterm", "terminal", "packer" }
 -- vim.g.tokyonight_dark_sidebar = false
 vim.cmd('colorscheme tokyonight')
