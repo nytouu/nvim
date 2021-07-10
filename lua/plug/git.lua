1b5e3ace00773941e9899ee43c9259722869c3c4
nytouu <capitaine7312@gmail.com>
2021-07-11 00:00:02 +0200

    gitsigns config

--- lua/plug/git.lua
+++ lua/plug/git.lua
@@ -3,11 +3,11 @@
 require'gitsigns'.setup{
     debug_mode = false,
     signs = {
-        add = {hl = "DiffAdd", text = "▌"},
-        change = {hl = "DiffChange", text = "▌"},
-        delete = {hl = "DiffDelete", text = "▌"},
-        topdelete = {hl = "DiffDelete", text = "▌"},
-        changedelete = {hl = "DiffChange", text = "▌"}
+        add = {hl = "DiffAdd", text = "▎"},
+        change = {hl = "DiffChange", text = "▎"},
+        delete = {hl = "DiffDelete", text = "▎"},
+        topdelete = {hl = "DiffDelete", text = "▎"},
+        changedelete = {hl = "DiffChange", text = "▎"}
     },
     numhl = false
 }
