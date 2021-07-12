d78c4d2c4e5d574678b0a8b1ba6b87193cbba39c
nytouu <capitaine7312@gmail.com>
2021-07-12 02:17:58 +0200

    dashboard conf

--- lua/plug/dashboard.lua
+++ lua/plug/dashboard.lua
@@ -1,12 +1,10 @@
 vim.g.dashboard_default_executive = 'telescope'
 
 vim.g.dashboard_custom_section = {
-	a = {description = {'  File Browser       spc f b'}, command = 'Telescope file_browser'},
+    a = {description = {'  Recent Files       spc f h'}, command = 'Telescope oldfiles'},
     b = {description = {'  Find File          spc f f'}, command = 'Telescope find_files'},
-    d = {description = {'  Recent Files       spc f h'}, command = 'Telescope oldfiles'},
-    e = {description = {'  Live Grep          spc f w'}, command = 'Telescope live_grep'},
-    f = {description = {'漣 Neovim Config      spc f c'}, command = 'Telescope find_files cwd=/home/nytou/.config/nvim'},
-    g = {description = {'  Quit               spc q q'}, command = ':q'}
+    c = {description = {'漣 Neovim Config      spc f c'}, command = 'Telescope find_files cwd=/home/nytou/.config/nvim'},
+    d = {description = {'  Quit               spc q q'}, command = ':q'}
 }
 
 vim.cmd "let packages = len(globpath('~/.local/share/nvim/site/pack/packer/start', '*', 0, 1))"
