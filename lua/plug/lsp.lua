f04161f4d3eafb8ba71d0315a37d4303ed0ae29c
nytouu <capitaine7312@gmail.com>
2021-07-12 21:39:46 +0200

    signature thing (not sure if it does anything a all)

--- lua/plug/lsp.lua
+++ lua/plug/lsp.lua
@@ -32,6 +32,9 @@ vim.fn.sign_define(
     {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"}
 )
 
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = "single",
+})
 -- vim.lsp.handlers["textDocument/signatureHelp"] = require'lspsaga'.signature_help
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
