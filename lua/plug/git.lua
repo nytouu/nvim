e8ef32eca28bcd508d00e2865dcd149d1130882d
nytouu <capitaine7312@gmail.com>
2021-07-12 02:18:08 +0200

    gitsigns conf more readable

--- lua/plug/git.lua
+++ lua/plug/git.lua
@@ -1,13 +1,26 @@
--- TODO colors not strong enough
-
-require'gitsigns'.setup{
+require'gitsigns'.setup {
     debug_mode = false,
     signs = {
-        add = {hl = "DiffAdd", text = "▎"},
-        change = {hl = "DiffChange", text = "▎"},
-        delete = {hl = "DiffDelete", text = "▎"},
-        topdelete = {hl = "DiffDelete", text = "▎"},
-        changedelete = {hl = "DiffChange", text = "▎"}
+        add = {
+			hl = "DiffAdd",
+			text = "▎"
+		},
+        change = {
+			hl = "DiffChange",
+			text = "▎"
+		},
+        delete = {
+			hl = "DiffDelete",
+			text = "▎"
+		},
+        topdelete = {
+			hl = "DiffDelete",
+			text = "▎"
+		},
+        changedelete = {
+			hl = "DiffChange",
+			text = "▎"
+		}
     },
     numhl = false
 }
