40b0d2be6e7830901fca6d8333218ee5702d973e
nytouu <capitaine7312@gmail.com>
2021-09-02 17:51:33 +0200

    cleanup again

--- lua/plug/autopairs.lua
+++ lua/plug/autopairs.lua
@@ -1,13 +1,7 @@
 require('nvim-autopairs').setup()
 
 require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = true -- automatically select the first item
+	map_cr = true, --  map <CR> on insert mode
+	map_complete = true, -- it will auto insert `(` after select function or method item
+	auto_select = true -- automatically select the first item
 })
-
--- Autocompletion and snippets
--- vim.api.nvim_set_keymap('i', '<CR>', 'v:lua.MUtils.completion_confirm()', {expr = true, noremap = true})
--- imap("<CR>", "v:lua.MUtils.completion_confirm()", {expr = true, noremap = true})
--- imap("<Tab>", "v:lua.MUtils.tab()", {expr = true, noremap = true})
--- imap("<S-Tab>", "v:lua.MUtils.s_tab()", {expr = true, noremap = true})
