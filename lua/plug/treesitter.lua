49fcce1a9ac9592d20f575470d8bf6a99a27b227
nytouu <capitaine7312@gmail.com>
2022-01-19 19:51:26 +0100

    add godot script lsp support

--- lua/plug/treesitter.lua
+++ lua/plug/treesitter.lua
@@ -2,6 +2,12 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
     ignore_install = {"haskell"},
     highlight = {
+		additional_vim_regex_highlighting = true,
         enable = true
+    },
+	indent = {
+		disable = {
+			"gdscript"
+		}
     }
 }
