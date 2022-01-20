14ac69d1e9ac63b3e8a873ee46b848171efc820c
nytouu <capitaine7312@gmail.com>
2022-01-21 00:38:15 +0100

    phpdoc is broken

--- lua/plug/treesitter.lua
+++ lua/plug/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
-    ignore_install = {"haskell","swift"},
+    ignore_install = {"haskell","swift","phpdoc"},
     highlight = {
 		additional_vim_regex_highlighting = true,
         enable = true
