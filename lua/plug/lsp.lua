69d5dad8ada511428b9a832e17cfdcbc8307dbce
nytouu <capitaine7312@gmail.com>
2022-01-21 00:39:31 +0100

    stop using old lsp servers in favor of new ones from nvim-lsp-installer

--- lua/plug/lsp.lua
+++ lua/plug/lsp.lua
@@ -1,125 +1,50 @@
-local lspconfig = require('lspconfig')
+local lsp_installer = require("nvim-lsp-installer")
 
-DATA_PATH = vim.fn.stdpath('data')
+local function on_attach(client, bufnr)
+  -- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
+    require "lsp_signature".on_attach({
+        bind = true,
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+        doc_lines = 5,
 
-local signaturecfg = {
-        bind = true, -- This is mandatory, otherwise border config won't get registered.
-                    -- If you want to hook lspsaga or other signature handler, pls set to false
-        doc_lines = 5, -- only show one line of comment set to 0 if you do not want API comments be shown
-
-        hint_enable = true, -- virtual hint enable
-        hint_prefix = "🐼 ",  -- Panda for parameter
+        hint_enable = true,
+        hint_prefix = "> ",
         hint_scheme = "String",
-        use_lspsaga = true,  -- set to true if you want to use lspsaga popup
+        use_lspsaga = true,
         handler_opts = {
-			border = "double"   -- double, single, shadow, none
+            border = "single"
         },
-        decorator = {"`", "`"}  -- or decorator = {"***", "***"}  decorator = {"**", "**"} see markdown signatureHelp
-    }
+        decorator = {"`", "`"}
+        })
+end
 
-local servers = {
-	{
-        name = 'vimls',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/vim/node_modules/.bin/vim-language-server", "--stdio"},
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-        }
-    },
-    {
-        name = 'gopls',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/go/gopls"},
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-        }
-    },
-	{
-        name = 'rls',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/rust/rust-analyzer"},
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-        }
-    },
-	{
-        name = 'yamlls',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/yaml/node_modules/.bin/yaml-language-server", "--stdio"},
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-        }
-    },
-	{
-        name = 'bashls',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/bash/node_modules/.bin/bash-language-server", "start"},
-            filetypes = { "sh", "zsh" },
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-        }
-    },
-	{
-        name = 'html',
-        config = {
-            -- capabilities.textDocument.completion.completionItem.snippetSupport = true
-            cmd = {"node", DATA_PATH .. "/lspinstall/html/vscode-html/html-language-features/server/dist/node/htmlServerMain.js", "--stdio"},
-            capabilities = capabilities,
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-        }
-    },
-	{
-        name = 'jsonls',
-        config = {
-            cmd = {
-                "node", DATA_PATH .. "/lspinstall/json/vscode-json/json-language-features/server/dist/node/jsonServerMain.js",
-                "--stdio",
-            },
+local function setup_handlers()
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
+        signs = true,
+        underline = true,
+        update_in_insert = true
 
-            commands = {
-                Format = {
-                    function()
-                        vim.lsp.buf.range_formatting({}, {0, 0}, {vim.fn.line("$"), 0})
-                    end
-                }
-            },
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
+    })
+end
+
+local enhance_server_opts = {
+    -- Provide settings that should only apply to the "eslintls" server
+    ["bashls"] = function(opts)
+        opts.settings = {
+            config = {
+                -- FIXME figure out why this one doesnt work but line 87 does
+                filetypes = { "sh", "zsh" }
+            }
         }
-    },
-	{
-        name = 'clangd',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd"},
-            handlers = {
-                ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-                    virtual_text = false,
-                    signs = true,
-                    underline = true,
-                    update_in_insert = true
-                })
-            },
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
+    end,
+    ["clangd"] = function(opts)
+        opts.settings = {
+            -- TODO add stuff here
         }
-    },
-	{
-		name = 'sumneko_lua',
-		config = {
-            cmd = {DATA_PATH .. "/lspinstall/lua/sumneko-lua-language-server", "-E", DATA_PATH .. "/lspinstall/lua/main.lua"},
-            settings = {
+    end,
+    ["sumneko_lua"] = function(opts)
+        opts.settings = {
                 Lua = {
                     runtime = {
                     -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
@@ -135,38 +60,41 @@ local servers = {
                     -- Make the server aware of Neovim runtime files
                     library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true},
                     maxPreload = 10000
-                    }
                 }
             },
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
-		}
-	},
-    {
-        name = 'pyright',
-        config = {
-            cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
-            settings = {
+        }
+    end,
+    ["pyright"] = function(opts)
+        opts.settings = {
                 python = {
                 analysis = {
                     typeCheckingMode = 'off',
                     autoSearchPaths = true,
                     useLibraryCodeForTypes = true
                     }
-                }
             },
-            on_attach = function(client, bufnr)
-                require "lsp_signature".on_attach(signaturecfg)  -- Note: add in lsp client on-attach
-            end
         }
-    }
+    end,
+    -- TODO add other servers
 }
 
-for _, server in ipairs(servers) do
-	local config = server.config or {}
-	lspconfig[server.name].setup(config)
-end
+setup_handlers()
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+        on_attach = on_attach,
+    }
+
+    if server.name == "bashls" then
+        opts.filetypes = {"sh", "zsh"}
+    end
+    if enhance_server_opts[server.name] then
+        enhance_server_opts[server.name](opts)
+    end
+
+    server:setup(opts)
+end)
+
 vim.fn.sign_define(
     "LspDiagnosticsSignError",
     {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"}
@@ -187,7 +115,6 @@ vim.fn.sign_define(
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 	border = "single",
 })
--- vim.lsp.handlers["textDocument/signatureHelp"] = require'lspsaga'.signature_help
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false,
