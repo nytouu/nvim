912986b7c826e8dad91afc60fea571c259e4898c
nytouu <capitaine7312@gmail.com>
2022-05-29 12:55:41 +0200

    neovide stuff

--- lua/settings.lua
+++ lua/settings.lua
@@ -51,9 +51,15 @@ vim.cmd('let g:vim_current_word#highlight_twins = 1')
 vim.cmd('let g:vim_current_word#highlight_delay = 0')
 
 -- neovide stuff
-vim.o.guifont = "JetBrainsMono Nerd Font:h18"
-vim.g.neovide_refresh_rate = 140
-vim.g.neovide_cursor_animation_length = 0
+vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+vim.g.neovide_refresh_rate = 144
+vim.g.neovide_cursor_animation_length = 0.03
+vim.g.neovide_cursor_trail_length=0.3
+vim.g.neovide_cursor_antialiasing=true
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_cursor_vfx_particle_lifetime=2.2
+vim.g.neovide_ccursor_vfx_particle_density=10.0
+vim.g.neovide_ccursor_vfx_particle_phase=3.0
 -- vim.cmd([[
 -- if !exists('g:neovide')
 -- 	autocmd VimEnter * hi Normal guibg=NONE
