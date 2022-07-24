999c47908ef5dfe68ce450f7ea4b3eeb40b8cef2
nytouu <capitaine7312@gmail.com>
2022-07-24 04:29:36 +0200

    keymappings

--- lua/keys.lua
+++ lua/keys.lua
@@ -17,16 +17,17 @@ keymap('n', '<C-j>', '<C-w>j', {silent = true})
 keymap('n', '<C-k>', '<C-w>k', {silent = true})
 keymap('n', '<C-l>', '<C-w>l', {silent = true})
 
+keymap('i', '<C-h>', '<Left>', {silent = true})
+keymap('i', '<C-l>', '<Right>', {silent = true})
+
 -- Terminal window navigation
 vim.cmd([[
 	tnoremap <C-h> <C-\><C-N><C-w>h
 	tnoremap <C-j> <C-\><C-N><C-w>j
 	tnoremap <C-k> <C-\><C-N><C-w>k
 	tnoremap <C-l> <C-\><C-N><C-w>l
-	inoremap <C-h> <C-\><C-N><C-w>h
 	inoremap <C-j> <C-\><C-N><C-w>j
 	inoremap <C-k> <C-\><C-N><C-w>k
-	inoremap <C-l> <C-\><C-N><C-w>l
 	tnoremap <Esc> <C-\><C-n>
 ]])
 
@@ -50,6 +51,8 @@ keymap('n', '<M-j>', ':move+<CR>',  {silent = true})
 keymap('x', '<M-k>', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true}) -- move multiple lines in visual
 keymap('x', '<M-j>', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
 
+keymap('i', '<C-j>', '\"\\<C-n>\"', {noremap = true, expr = true})
+keymap('i', '<C-k>', '\"\\<C-p>\"', {noremap = true, expr = true})
 -- Better nav for omnicomplete
-vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
-vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+-- vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
+-- vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
