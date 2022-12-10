0b932914862f775378b91e9e57fbcc39bd847b33
nytouu <capitaine7312@gmail.com>
2022-12-11 00:19:44 +0100

    nonicons for nvimtree

--- lua/plug/nvim-tree.lua
+++ lua/plug/nvim-tree.lua
@@ -1,9 +1,16 @@
+local nonicons_extention = require("nvim-nonicons.extentions.nvim-tree")
+
 require'nvim-tree'.setup {
 	disable_netrw       = true,
 	hijack_netrw        = true,
 	open_on_setup       = false,
 	ignore_ft_on_setup  = {},
 	open_on_tab         = false,
+	renderer = {
+		icons = {
+			glyphs = nonicons_extention.glyphs,
+		}
+	},
 	view = {
 		width = 30,
 		side = 'left',
