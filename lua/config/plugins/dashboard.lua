596d5b8c407defeaf18b64104ae0e7d0dc666d9c
nytouu <capitaine7312@gmail.com>
2023-01-10 16:19:31 +0100

    dashboard config

--- lua/config/plugins/dashboard.lua
+++ lua/config/plugins/dashboard.lua
@@ -21,7 +21,8 @@ db.custom_header = {
 '                                 ',
 }
 
-db.custom_footer = { "lazy loaded " .. stats.count .. " plugins in " .. stats.startuptime .. "ms"}
+-- db.custom_footer = { "lazy loaded " .. stats.count .. " plugins in " .. stats.startuptime .. "ms"}
+db.custom_footer = { "lazy loaded " .. stats.count .. " plugins" }
 db.custom_center = {
     {icon = '  ',
     desc = 'Recent Files                            ',
@@ -41,32 +42,7 @@ db.custom_center = {
     shortcut = 'spc q q'},
 }
 
-
-
-
-
---[[
-vim.g.dashboard_default_executive = 'telescope'
-
-vim.g.dashboard_custom_section = {
-    a = {description = {'  Recent Files       spc f h'}, command = 'Telescope oldfiles'},
-    b = {description = {'  Find File          spc f f'}, command = 'Telescope find_files'},
-    c = {description = {'漣 Neovim Config      spc f c'}, command = 'Telescope find_files cwd=$HOME/.config/nvim'},
-    d = {description = {'  Quit               spc q q'}, command = ':q'}
-}
-
-vim.cmd "let packages = len(globpath('~/.local/share/nvim/site/pack/packer/start', '*', 0, 1))"
-vim.api.nvim_exec(
-]]--
--- [[
-    -- let g:dashboard_custom_footer = ['packer loaded '..packages..' plugins']
--- ]],
--- false
--- )
-
---[[
-
-vim.g.dashboard_custom_header =
+-- vim.g.dashboard_custom_header =
 -- {
 -- '                                                  ',
 -- '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠴⠶⠶⠤⣄⣀⣀⣀⣀⡴⠛⡶⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
@@ -103,4 +79,3 @@ vim.g.dashboard_custom_header =
 -- '             hydra               '
 -- }
 --
-]]--
