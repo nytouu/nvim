eb66044236fe92d1ffe6e4087503aab383601d02
nytouu <capitaine7312@gmail.com>
2023-01-22 16:02:21 +0100

    gdscript config

--- ftdetect/gdscript.lua
+++ ftdetect/gdscript.lua
@@ -1,11 +1,11 @@
--- require('lspconfig').gdscript.setup{
---     on_attach = on_attach,
---     cmd = {
--- 		'nc','localhost','6008'
--- 	},
---     flags = {
---         debounce_text_changes = 150,
---     }
--- }
--- 
--- vim.cmd('autocmd BufEnter *.gd,*.gdscript :lua vim.api.nvim_buf_set_option(0, "commentstring", "# %s")')
+require('lspconfig').gdscript.setup{
+    on_attach = on_attach,
+    cmd = {
+		'nc','localhost','6005'
+	},
+    flags = {
+        debounce_text_changes = 150,
+    }
+}
+
+vim.cmd('autocmd BufEnter *.gd,*.gdscript :lua vim.api.nvim_buf_set_option(0, "commentstring", "# %s")')
