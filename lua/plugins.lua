26c97f56806c2d1de4eacde97bb68c5c162bd8d8
nytouu <capitaine7312@gmail.com>
2023-02-10 23:24:19 +0100

    move hlslens config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -225,19 +225,7 @@ return {
 	{
 		"kevinhwang91/nvim-hlslens",
 		config = function()
-			require("hlslens").setup({
-				-- override_lens = function() end,
-				build_position_cb = function(plist, _, _, _)
-					require("scrollbar.handlers.search").handler.show(plist.start_pos)
-				end,
-			})
-
-			vim.cmd([[
-				augroup scrollbar_search_hide
-				autocmd!
-				autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
-				augroup END
-			]])
+			require("config.plugins.hlslens")
 		end,
 		event = "VimEnter"
 	},
