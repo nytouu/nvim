8d24da6d426c8399a65b2cc6a5c6dcf270ec9568
nytouu <capitaine7312@gmail.com>
2023-02-10 23:14:42 +0100

    transparent config

---
+++ lua/config/plugins/transparent.lua
@@ -0,0 +1,15 @@
+require("transparent").setup({
+	-- enable = true,
+	extra_groups = {
+		"BufferLineTabClose",
+		"BufferlineBufferSelected",
+		"BufferLineFill",
+		"BufferLineBackground",
+		"BufferLineSeparator",
+		"BufferLineIndicatorSelected",
+		"ScrollHead",
+		-- "ScrollBody",
+		"ScrollTail"
+	},
+	exclude = {},
+})
