b49c306360d0c281118759cf2c801ded6e350398
nytouu <capitaine7312@gmail.com>
2023-03-20 19:25:06 +0100

    treesitter config

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -2,9 +2,12 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
     ignore_install = {"haskell","swift","phpdoc","ruby"},
     highlight = {
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
         enable = true
     },
+	autotag = {
+		enable = true,
+	},
 	indent = {
 		disable = {
 			"gdscript"
