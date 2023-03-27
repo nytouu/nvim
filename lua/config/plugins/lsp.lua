d89a405a1bef859f6cd41d035449cf4f3bf07fb1
nytouu <capitaine7312@gmail.com>
2023-03-28 00:07:23 +0200

    lsp ensure installed servers

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,5 +1,22 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup{
+	ensure_installed = {
+		"lua_ls",
+		"rust_analyzer",
+		"bashls",
+		"clangd",
+		"cmake",
+		"cssls",
+		"html",
+		"jsonls",
+		"marksman",
+		"pyright",
+		"tsserver",
+		"vimls",
+		"yamlls"
+	},
+	automatic_installation = true,
+}
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
