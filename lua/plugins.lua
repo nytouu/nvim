aa18999a3ab203e71a021bee7eafa6c428e4e686
nytouu <capitaine7312@gmail.com>
2023-03-28 23:06:26 +0200

    nvim surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -347,5 +347,14 @@ return {
 		end,
 		event = "VeryLazy",
 	},
-	-- { "chrisgrieser/nvim-spider" },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
