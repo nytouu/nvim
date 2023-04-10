5bb7fc6bbc907a14b4e43f7a8d2a95c4a299cdb9
nytouu <capitaine7312@gmail.com>
2023-04-10 21:24:15 +0200

    cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -352,9 +352,7 @@ return {
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
 		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
+			require("nvim-surround").setup()
 		end,
 	},
 }
