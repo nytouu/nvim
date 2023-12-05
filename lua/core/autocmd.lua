90b7ab15140ca72ee99fbb5869515e64cc4f165b
nytouu <capitaine7312@gmail.com>
2023-12-05 23:34:03 +0100

    autocmds

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -8,26 +8,50 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "dashboard", "neo-tree", "norg" },
+	pattern = { "dashboard", "neo-tree", "norg", "outline" },
 	callback = function()
-		require("ufo").detach()
-		vim.opt_local.foldenable = false
+        local ufo_status = pcall(require, "ufo")
+        if ufo_status then
+            require("ufo").detach()
+            vim.opt_local.foldenable = false
+        end
+
+        vim.opt_local.winbar = nil
 	end
 })
 
-local function augroup(name, autocmds)
-	local group = vim.api.nvim_create_augroup(name, {})
-	for event, opts in pairs(autocmds) do
-		opts.group = group
-		vim.api.nvim_create_autocmd(event, opts)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"js",
+		"ts",
+		"nix"
+	},
+	callback = function()
+		vim.opt.shiftwidth = 2
+		vim.opt.softtabstop = 2
+		vim.opt.tabstop = 2
+		vim.opt.listchars:append("leadmultispace:· ")
 	end
-end
-
--- Set cursor hints according to mode
-augroup('Enter', {
-	InsertEnter = { command = 'set cursorline' },
 })
 
-augroup('Leave', {
-	InsertLeave = { command = 'set nocursorline' },
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "norg" },
+	callback = function()
+        vim.opt.list = false
+		vim.opt.spell = true
+		vim.opt.spelllang = { 'en_us', 'fr' }
+	end
+})
+
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function ()
+        vim.wo.cursorline = true
+    end
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function ()
+        vim.wo.cursorline = false
+    end
 })
