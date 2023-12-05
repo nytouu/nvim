5188ad7e9f3593a062e90a36461c7c5a7ecb979f
nytouu <capitaine7312@gmail.com>
2023-12-05 23:33:53 +0100

    dashboard config

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvimdev/dashboard-nvim",
+		enabled = true,
 		config = function()
 
             local stats = require("lazy").stats()
@@ -35,35 +36,60 @@ return {
                             desc = "Find Files                              ",
                             key = "spc f f",
                             action = "Telescope find_files",
+							icon_hl = 'Title',
+							desc_hl = 'String',
+							key_hl = 'Number',
                         },
                         {
                             icon = "  ",
                             desc = "Find Word                              ",
                             key = "spc f w",
                             action = "Telescope live_grep",
+							icon_hl = 'Title',
+							desc_hl = 'String',
+							key_hl = 'Number',
                         },
                         {
                             icon = "  ",
                             desc = "Recent Files                            ",
                             key = "spc f r",
                             action = "Telescope oldfiles",
+							icon_hl = 'Title',
+							desc_hl = 'String',
+							key_hl = 'Number',
                         },
                         {
                             icon = "󰢻  ",
                             desc = "Neovim Config                           ",
                             key = "spc f c",
                             action = "Telescope find_files cwd=$HOME/.config/nvim",
+							icon_hl = 'Title',
+							desc_hl = 'String',
+							key_hl = 'Number',
                         },
                         {
                             icon = "  ",
                             desc = "Quit                                    ",
                             key = "spc q q",
                             action = ":q",
+							icon_hl = 'Title',
+							desc_hl = 'String',
+							key_hl = 'Number',
                         },
                     },
-                    footer = { "lazy loaded " .. stats.count .. " plugins" },
+                    footer = {
+						"",
+						" " .. os.date('%Y-%m-%d'),
+						" " .. os.date('%H:%M'),
+						"",
+						"lazy loaded " .. stats.count .. " plugins" ,
+					},
                 },
             })
+			local hi = vim.api.nvim_set_hl
+
+			hi(0, "DashboardHeader", {fg = "#ca98a3"})
+			hi(0, "DashboardFooter", {fg = "#c4995b"})
 		end,
 		event = "VimEnter",
 	},
