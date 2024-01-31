d8f0feefafb7b856dcf82b4f2eb43a8ab5bb7311
nytouu <capitaine7312@gmail.com>
2024-01-31 11:44:03 +0100

    add treesitter context

---
+++ lua/plugins/context.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-treesitter/nvim-treesitter-context",
+    enabled = true,
+	keys = {
+		{ "<leader>tC", "<cmd>TSContextToggle<cr>", desc = "Toggle treesitter context" },
+	},
+    event = { "BufEnter" },
+	opts = {
+        enable = true,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = false,
+        multiline_threshold = 20,
+        trim_scope = 'outer',
+        mode = 'cursor',
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+    }
+}
