97ca4b542f930308b60db96b8921393237427bd9
nytouu <capitaine7312@gmail.com>
2024-01-31 11:43:48 +0100

    snippet/cmp mappings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,6 +18,22 @@ return {
 
 		local luasnip = require("luasnip")
 
+		vim.keymap.set({"i"}, "<Tab>", function() luasnip.expand() end, {silent = true})
+		vim.keymap.set({"i", "s"}, "<Tab>", function() luasnip.jump( 1) end, {silent = true})
+		vim.keymap.set({"i", "s"}, "<S-Tab>", function() luasnip.jump(-1) end, {silent = true})
+
+		vim.keymap.set({"i", "s"}, "<C-E>", function()
+			if luasnip.choice_active() then
+				luasnip.change_choice(1)
+			end
+		end, {silent = true})
+
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -38,24 +54,27 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				["<Tab>"] = function(fallback)
+				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
-						cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+						cmp.select_next_item()
 					elseif require("luasnip").expand_or_jumpable() then
-						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+						require("luasnip").expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
-				end,
-				["<S-Tab>"] = function(fallback)
+				end, {"i", "s"}),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
-						cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+						cmp.select_prev_item()
 					elseif require("luasnip").jumpable(-1) then
-						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+						require("luasnip").jump(-1)
 					else
 						fallback()
 					end
-				end,
+				end, {"i", "s"})
 			}),
 
 			-- sources for autocompletion
