5d5741ca4f362f1193c2c0bd07aa96a4b5c14108
nytouu <capitaine7312@gmail.com>
2024-02-28 22:49:47 +0100

    lsp + none

---
+++ lua/plugins/lsp/none-ls.lua
@@ -0,0 +1,29 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"stylua",
+				"csharpier",
+				"black",
+			},
+			automatic_installation = true,
+			handlers = {},
+		})
+
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			-- sources = {
+			-- 	null_ls.builtins.formatting.clang_format.with({
+			-- 		extra_args = { "-style", "Microsoft" },
+			-- 	}),
+			-- },
+		})
+	end,
+}
