b2095cc961a19495e22577d8d757eb599cdd6b22
nytouu <capitaine7312@gmail.com>
2024-05-12 23:21:35 +0200

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,36 @@
 return {
-	{
-		"savq/melange-nvim",
-		priority = 1000,
-		enabled = true,
-		config = function()
-			local hi = vim.api.nvim_set_hl
+    {
+        "savq/melange-nvim",
+        priority = 1000,
+        enabled = false,
+        config = function()
+            local hi = vim.api.nvim_set_hl
 
-			hi(0, "WinSeparator", {fg = "#34302c", bg = "#34302c"})
-			hi(0, "NeoTreeWinSeparator", {fg = "#34302c", bg = "#34302c"})
-			hi(0, "NeoTreeStatusLine", {fg = "#34302c", bg = "#34302c"})
-			hi(0, "NeoTreeStatusLineNC", {fg = "#34302c", bg = "#34302c"})
-			hi(0, "NeoTreeExpander", {fg = "#eabf6d", bg = "NONE"})
-			hi(0, "StatusLine", {fg = "#34302c", bg = "#34302c"})
-			hi(0, "StatusLineNC", {fg = "#34302c", bg = "#34302c"})
-			hi(0, "Normal", {bg = "NONE"})
-			-- hi(0, "WinBarNC", {bg = "NONE"})
-			-- hi(0, "NonText", {fg = "#403a36"})
-			-- hi(0, "SpecialKey", {fg = "#403a36"})
+            hi(0, "WinSeparator", { fg = "#34302c", bg = "#34302c" })
+            hi(0, "NeoTreeWinSeparator", { fg = "#34302c", bg = "#34302c" })
+            hi(0, "NeoTreeStatusLine", { fg = "#34302c", bg = "#34302c" })
+            hi(0, "NeoTreeStatusLineNC", { fg = "#34302c", bg = "#34302c" })
+            hi(0, "NeoTreeExpander", { fg = "#eabf6d", bg = "NONE" })
+            hi(0, "StatusLine", { fg = "#34302c", bg = "#34302c" })
+            hi(0, "StatusLineNC", { fg = "#34302c", bg = "#34302c" })
+            -- hi(0, "NeoTreeVertSplit", {bg = "NONE"})
+            -- hi(0, "WinBarNC", {bg = "NONE"})
+            -- hi(0, "NonText", {fg = "#403a36"})
+            -- hi(0, "SpecialKey", {fg = "#403a36"})
 
-			vim.cmd[[colorscheme melange]]
-		end,
-	},
+            vim.o.background = "dark"
+            vim.cmd.colorscheme("melange")
+        end,
+    },
+    {
+        "Mofiqul/adwaita.nvim",
+        lazy = false,
+        enabled = true,
+        priority = 1000,
+        config = function()
+            vim.o.background = "dark"
+            vim.g.adwaita_disable_cursorline = true
+            vim.cmd.colorscheme("adwaita")
+        end
+    }
 }
