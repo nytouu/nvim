cf131881f356e2975a1a59da50b72da8795b9666
nytouu <capitaine7312@gmail.com>
2024-09-24 23:53:40 +0200

    gitsigns config

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,11 +1,27 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		require('gitsigns').setup {
+			signs                        = {
+				untracked    = { text = '┃' },
+			},
+			signs_staged                 = {
+				untracked    = { text = '┃' },
+			},
+			current_line_blame_opts      = {
+				delay = 0,
+			},
+			preview_config               = {
+				border = 'solid',
+			},
+		}
+	end,
 	keys = {
-		{ "<leader>gj", "<cmd>Gitsigns next_hunk<cr>", desc = "Next hunk" },
-		{ "<leader>gk", "<cmd>Gitsigns prev_hunk<cr>", desc = "Previous hunk" },
-		{ "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", desc = "Preview hunk" },
-		{ "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Reset hunk" },
+		{ "<leader>gj", "<cmd>Gitsigns next_hunk<cr>",                 desc = "Next hunk" },
+		{ "<leader>gk", "<cmd>Gitsigns prev_hunk<cr>",                 desc = "Previous hunk" },
+		{ "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>",              desc = "Preview hunk" },
+		{ "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>",                desc = "Reset hunk" },
+		{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Show current line blame" },
 	},
 }
