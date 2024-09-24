90b0e24b256e889d75f7eee32daf71d10df2bed3
nytouu <capitaine7312@gmail.com>
2024-09-24 13:16:56 +0200

    remove bufferline stuff

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -4,14 +4,16 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	-- version = "*",
 	keys = {
-		{ "<S-k>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-		{ "<S-j>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		-- { "<S-k>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+		-- { "<S-j>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		-- { "<S-k>", "<cmd>bprevious<cr>", desc = "Prev buffer" },
+		-- { "<S-j>", "<cmd>bnext<cr>", desc = "Next buffer" },
 	},
 	opts = {
 		options = {
 			themable = true,
 			diagnostics = "nvim_lsp",
-			mode = "buffers",
+			mode = "tabs",
 			separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
 			tab_size = 16,
 			show_tab_indicators = true,
