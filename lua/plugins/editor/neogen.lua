d22976a18e68e7951ddcf322d1fe8388b64776c4
nytouu <capitaine7312@gmail.com>
2024-10-06 15:44:05 +0200

    add neogen

---
+++ lua/plugins/editor/neogen.lua
@@ -0,0 +1,9 @@
+return {
+	"danymat/neogen",
+	keys = {
+		{ "<leader>df", "<cmd>Neogen func<cr>", desc = "Generate function doc" },
+		{ "<leader>dc", "<cmd>Neogen class<cr>", desc = "Generate class doc" },
+		{ "<leader>dt", "<cmd>Neogen ", desc = "Generate type doc" },
+	},
+	config = true;
+}
